09ac1bec6d96313ed8b8093e890e704ac6e4f330
Akrombek Odilov <00012827@wiut.uz>
2021-03-05 17:46:59 +0500

    add stlyes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.use('/static', express.static('public'))
+
 app.set('view engine', 'pug')
 app.get('/', (req, res) => {
-	//todo
+	res.render('home')
 })
 
 app.listen(8000, err => {
