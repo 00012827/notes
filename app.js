4973e73f06fcb20dfb6d68c5127f07969c15d86d
Akrombek Odilov <00012827@wiut.uz>
2021-03-10 15:06:14 +0500

    init

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
 	res.render('home')
 })
 
+app.get('/create-note', (req, res) => {
+	res.render('create')
+})
+
 app.listen(8000, err => {
 	if(err) throw err
 		console.log('App is running...')
