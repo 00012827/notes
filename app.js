0a1aac295657206af32d6fe533a7fc41b8787534
Akrombek Odilov <00012827@wiut.uz>
2021-03-17 14:12:43 +0500

    add some page

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ app.get('/create-note', (req, res) => {
 	res.render('create')
 })
 
+app.get('/notes', (req, res)=> {
+	res.render('notes', { notes:['First note', 'Second note'] })
+})
+
+
+app.get('/notes/detail', (req, res) =>{
+	res.render('detail')
+})
+
 app.listen(8000, err => {
 	if(err) throw err
 		console.log('App is running...')
